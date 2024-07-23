5adb4f11b29fd088adae27f6dcaf54a152fb69d8
Jost Hötker <65763949+jost-hoetker@users.noreply.github.com>
2024-07-24 00:24:20 +0200

    Update script.js
    Mobile fix

--- script.js
+++ script.js
@@ -39,28 +39,3 @@ document.body.addEventListener('click', function() {
     }
 });
 
-// Optional: Handle touch events for mobile devices
-document.body.addEventListener('touchstart', function() {
-    const audio = document.getElementById('audio');
-    const horn = document.getElementById('horn');
-    const textElement = document.getElementById('text');
-    const coords = document.getElementById('coords');
-    const imageElement = document.getElementById('spinning-image');
-    const druecken = document.getElementById('druecken');
-
-    if (audio.paused) {
-        audio.play();
-        horn.play();
-        if (textElement.classList.contains('hidden')) {
-            textElement.classList.remove('hidden');
-            coords.classList.remove('hidden');
-            imageElement.classList.remove('hidden');
-            imageElement.classList.add('spinning-image');
-            druecken.classList.add('hidden');
-        } else {
-            textElement.classList.add('hidden');
-            coords.classList.add('hidden')
-            imageElement.add('hidden');
-        }
-    }
-});
